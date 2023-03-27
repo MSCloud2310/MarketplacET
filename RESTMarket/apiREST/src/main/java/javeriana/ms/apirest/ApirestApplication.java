ef3b96cd355ce322790d663c5c3d8d74eca4e3a7
txusdavid27 <jetraslavina@javeriana.edu.co>
2023-03-27 03:44:15 -0500

    Cliente y Api

---
+++ RESTMarket/apiREST/src/main/java/javeriana/ms/apirest/ApirestApplication.java
@@ -0,0 +1,16 @@
+package javeriana.ms.apirest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+
+public class ApirestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApirestApplication.class, args);
+	}
+
+}
---
+++ RESTMarket/apiREST/src/test/java/javeriana/ms/apirest/ApirestApplicationTests.java
@@ -0,0 +1,13 @@
+package javeriana.ms.apirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
