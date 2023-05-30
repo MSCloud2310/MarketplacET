4fbb3e0b5c9e194365187c5dc133886ee4624e08
Mauren <mauren.rivera.16@hotmail.com>
2023-05-29 20:56:47 -0500

    Cambios en la API Rest

--- Gateway/apiREST/src/main/java/javeriana/ms/apirest/ApirestController.java
+++ Gateway/apiREST/src/main/java/javeriana/ms/apirest/ApirestController.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.loadbalancer.annotation.LoadBalancerClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,12 +15,13 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
-@LoadBalancerClient
 
-@RestController
+@RestController()
+@RequestMapping("/api")
 public class ApirestController {
     @Autowired
     RestTemplate restTemplate;
@@ -30,6 +30,11 @@ public class ApirestController {
     @LoadBalanced
     RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @GetMapping()
+    public String login() {
+        return "Hello world!";
     }
 
     @PostMapping("/login")
@@ -65,28 +70,28 @@ public class ApirestController {
         return response;
     }
 
-    @GetMapping("/items")
-    public String traerTodo(@RequestParam Long id) {
-        String response = restTemplate.getForObject("http://servicios/servicio/" + id, String.class);
+    @GetMapping("/servicios")
+    public String traerTodo(@RequestParam String servicio) {
+        String response = restTemplate.getForObject("http://servicios/servicio/"+servicio, String.class);
         return response;
     }
 
     @GetMapping("/categoria")
     public String traerCategoria(@RequestParam String nombre) {
-        String response = restTemplate.getForObject("http://servicioOrden/servicio/categoria/"+nombre, String.class, nombre);
+        String response = restTemplate.getForObject("http://servicios/servicio/categoria/"+nombre, String.class,nombre);
         return response;
     }
 
     /*BUSCAR POR CADENA DE TEXTO */
     @GetMapping("/buqueda-items")
     public String traerCoincidencias(@RequestParam String buscar) {
-        String response = restTemplate.getForObject("http://servicioOrden/servicio/buscar/"+buscar, String.class, buscar);
+        String response = restTemplate.getForObject("http://servicios/servicio/buscar/"+buscar, String.class,buscar);
         return response;
     }
 
-    @GetMapping("/item")
-    public String traerUno(@RequestParam int item) {
-        String response = restTemplate.getForObject("http://servicioOrden/servicio/"+item, String.class, item);
+    @GetMapping("/servicio")
+    public String traerUno(@PathVariable int servicio) {
+        String response = restTemplate.getForObject("http://servicios/servicio/"+servicio, String.class,servicio);
         return response;
     }
 
@@ -105,21 +110,14 @@ public class ApirestController {
     HttpHeaders headers = new HttpHeaders();
     headers.setContentType(MediaType.APPLICATION_JSON);
     HttpEntity<String> requestEntity = new HttpEntity<String>(requestBody, headers);
-    ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://facturacion/facturacion", requestEntity, String.class);
+    ResponseEntity<String> responseEntity = restTemplate.postForEntity("http://facturacion/orden", requestEntity, String.class);
     String response = responseEntity.getBody();
     return response;
     }
-/* 
-    @GetMapping("/carrito")
-    public String traerCarrito() {
-        String response = restTemplate.getForObject("http://servicioProductos/carrito", String.class);
-        return response;
-    }
-*/
+
     @DeleteMapping("/carrito/{id}")
     public ResponseEntity<String> comprarProducto(@PathVariable int id) {
-        restTemplate.delete("http://facturacion/facturacion/{id}", id);
+        restTemplate.delete("http://facturacion/orden/{id}", id);
         return new ResponseEntity<>("Producto eliminado correctamente", HttpStatus.OK);
     }
-
 }
